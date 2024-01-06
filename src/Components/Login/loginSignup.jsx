29873960c9e37aa154050eaa5601351cdb2d1945
VISHAL DUBEY <dubeyv571@gmail.com>
2024-01-06 11:08:15 +0530

    login page is modified

--- src/Components/Login/loginSignup.jsx
+++ src/Components/Login/loginSignup.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import './loginSignup.css';
+import googleLogo from '../Assests/google_logo.png';
 const LoginSignup = () => {
     return <>
         <div className="loginsignup-container">
             <div className="wrapper">
                 <form action="">
-                    <h1>Log in</h1>
+                    <h1>Login / Register</h1>
                     <div className="loginsignup-fields">
                         <div className="input-box">
-                            <label for="phone" id="rr">Enter your mobile number</label>
+                            <label for="phone" id="rr">Mobile Number</label>
                             <input id="name" name="phone" placeholder="Enter your Number" required />
                         </div>
 
-
-                        <button id="continue" type="submit">Continue</button>
-
+                        {/* <div className="btn"> */}
+                        <button  id="continue" type="submit">Continue</button>
+                        {/* </div> */}
+                        <div className="line-container">
                         <div className="line"></div>
-                        <button id="google" type="submit"> Login with Google</button>
-                        <div className="terms">
-                            <label id="text"><input type="checkbox" />By Continuing, you agree to D-Fit Nutrition terms of use and privacy policy. </label>
+                        <div className="line-text">Or</div>
+                        <div className="line"></div>
                         </div>
-
-
-
+                        <button id="google" type="submit"> 
+                            <div className="google-logo-container">
+                                <img src={googleLogo} alt="Google Logo" className="google-logo" />
+                            </div>
+                            Login with Google
+                        </button>
+                        <div className="terms">
+                            <label id="text">
+                                <input id="ck" type="checkbox" />
+                                <span className="custom-checkbox"></span>
+                                By Continuing, you agree to D-Fit Nutrition terms of use and privacy policy.
+                            </label>
+                        </div>  
                         <div className="register-link">
                             <p>Don't have an account? <a href=" ">Register</a></p>
                         </div>
